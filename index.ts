67f55d8f452b8542daa5d8c8728d1c30af4c0952
Igor Graziano <igorrsgraziano@gmail.com>
2023-11-19 01:18:04 -0300

    fixed riff header webp error

--- index.ts
+++ index.ts
@@ -1,6 +1,14 @@
-import { Client, MessageMedia } from "whatsapp-web.js";
+import * as WhatsApp from "whatsapp-web.js";
 import * as QRCode from "qrcode-terminal";
-const client = new Client({});
+import quotes from "./quotes";
+import sharp from "sharp";
+
+const client = new WhatsApp.Client({
+	authStrategy: new WhatsApp.LocalAuth({ dataPath: "./auth" }),
+	puppeteer: {
+		headless: false,
+	},
+});
 
 client.on("qr", (qr) => {
 	console.log("QR RECEIVED");
@@ -12,15 +20,30 @@ client.on("ready", () => {
 });
 
 client.on("message", async (msg) => {
-	//Receiving image
-	if (msg.hasMedia) {
-		const media = await msg.downloadMedia();
-		//Return sticker
-		if (msg.type === "image") {
-			const sticker = new MessageMedia("image/webp", media.data, "sticker");
+	try {
+		//Receiving image
+		if (msg.hasMedia && msg.type === "image") {
+			//Return sticker
+			//Response media type
+			const media = await msg.downloadMedia();
+			console.log(media);
+			const imgBuffer = Buffer.from(media.data, "base64");
+			const mediaWebp = await sharp(imgBuffer).webp().toBuffer();
+			const sticker = new WhatsApp.MessageMedia("image/webp", mediaWebp.toString("base64"), "sticker");
 			const chat = await msg.getChat();
-			chat.sendMessage(sticker, { sendMediaAsSticker: true });
+			const quote = quotes[Math.floor(Math.random() * quotes.length)];
+			await chat.sendMessage(sticker, { sendMediaAsSticker: true, stickerAuthor: `${quote.source} - ${quote.philosophy}`, stickerName: quote.quote });
+			// chat.sendMessage(sticker, { sendMediaAsSticker: true, stickerAuthor: `${quote.source} - ${quote.philosophy}` });
+			// chat.sendMessage(sticker, { sendMediaAsSticker: true,  stickerName: quote.quote });
+
+			// await new Promise((resolve) => setTimeout(resolve, 2000));
+			// const media = await msg.downloadMedia();
+			// const sticker = new WhatsApp.MessageMedia("image/webp", media.data, "sticker");
+			// const chat = await msg.getChat();
+			// chat.sendMessage(sticker, { sendMediaAsSticker: true });
 		}
+	} catch (error) {
+		console.log(error);
 	}
 });
 
