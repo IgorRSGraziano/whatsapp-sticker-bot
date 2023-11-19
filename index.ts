3ca616d6cf802d3ff55bd2a546730015bfd5369c
Igor Graziano <igorrsgraziano@gmail.com>
2023-11-18 23:02:59 -0300

    created respond has sticker

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { Client } from "whatsapp-web.js";
+import { Client, MessageMedia } from "whatsapp-web.js";
 import * as QRCode from "qrcode-terminal";
 const client = new Client({});
 
@@ -9,6 +9,19 @@ client.on("qr", (qr) => {
 
 client.on("ready", () => {
 	console.log("Client is ready!");
+});
+
+client.on("message", async (msg) => {
+	//Receiving image
+	if (msg.hasMedia) {
+		const media = await msg.downloadMedia();
+		//Return sticker
+		if (msg.type === "image") {
+			const sticker = new MessageMedia("image/webp", media.data, "sticker");
+			const chat = await msg.getChat();
+			chat.sendMessage(sticker, { sendMediaAsSticker: true });
+		}
+	}
 });
 
 client.initialize();
