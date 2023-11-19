9fc039240d2dbb88346ebbcc06e872c77f97bbc5
Igor Graziano <igorrsgraziano@gmail.com>
2023-11-18 22:46:57 -0300

    created initial login

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!");
+import { Client } from "whatsapp-web.js";
+import * as QRCode from "qrcode-terminal";
+const client = new Client({});
+
+client.on("qr", (qr) => {
+	console.log("QR RECEIVED");
+	QRCode.generate(qr, { small: true });
+});
+
+client.on("ready", () => {
+	console.log("Client is ready!");
+});
+
+client.initialize();
