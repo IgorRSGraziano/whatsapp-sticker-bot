cbcff053a429b676621350c46789fdd5cf3bb963
Igor Graziano <igorrsgraziano@gmail.com>
2024-04-25 23:01:00 -0300

    temp handle for #2885 whatsappwebjs issue

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import WhatsApp from "whatsapp-web.js";
-import QRCode from "qrcode-terminal";
-import quotes from "./quotes.js";
-import sharp from "sharp";
 import fs from "fs";
+import QRCode from "qrcode-terminal";
+import sharp from "sharp";
+import WhatsApp from "whatsapp-web.js";
+import quotes from "./quotes.js";
 
 process.env.TEMP_DIR ??= "./temp/";
 
@@ -12,6 +12,10 @@ if (!fs.existsSync(process.env.TEMP_DIR)) {
 
 const client = new WhatsApp.Client({
 	authStrategy: new WhatsApp.LocalAuth({ dataPath: process.env.TEMP_DIR + "/auth" }),
+	webVersionCache: {
+		remotePath: "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.3000.1013063077-alpha.html",
+		type: "remote",
+	},
 	puppeteer: {
 		headless: true,
 		args: ["--no-sandbox", "--disable-setuid-sandbox"],
@@ -40,7 +44,7 @@ client.on("message", async (msg) => {
 			const sticker = new WhatsApp.MessageMedia("image/webp", mediaResized.toString("base64"), "sticker");
 			const chat = await msg.getChat();
 			const quote = quotes[Math.floor(Math.random() * quotes.length)];
-			await chat.sendMessage(sticker, { sendMediaAsSticker: true, stickerAuthor: `${quote.source} - ${quote.philosophy}`, stickerName: quote.quote });
+			await chat.sendMessage(sticker, { sendMediaAsSticker: true, stickerAuthor: `${quote.source} - ${quote.philosophy}`, stickerName: quote.quote, quotedMessageId: msg.id._serialized });
 		}
 	} catch (error) {
 		console.error(error);
