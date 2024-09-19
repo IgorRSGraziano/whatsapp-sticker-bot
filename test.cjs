f7f6e331d5723f702b2305ab9e3b71139f383e87
Igor Graziano <igorrsgraziano@gmail.com>
2024-09-19 20:43:10 -0300

    updated deps and fix version issue

---
+++ test.cjs
@@ -0,0 +1,20 @@
+const { Client } = require('whatsapp-web.js');
+
+const client = new Client();
+
+client.on('qr', (qr) => {
+    // Generate and scan this code with your phone
+    console.log('QR RECEIVED', qr);
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', msg => {
+    if (msg.body == '!ping') {
+        msg.reply('pong');
+    }
+});
+
+client.initialize();
